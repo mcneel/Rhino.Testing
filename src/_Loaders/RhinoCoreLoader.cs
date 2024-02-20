362705aa50ef97b58d6a1f2c1020d9c0ab34183f
eirannejad <eirannejad@gmail.com>
2024-02-20 16:11:46 -0600

    ensure rdk is loaded and upped to 8.0.6

--- src/_Loaders/RhinoCoreLoader.cs
+++ src/_Loaders/RhinoCoreLoader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace Rhino.Testing
 {
@@ -15,6 +16,16 @@ namespace Rhino.Testing
 #endif
 
             s_core = new Rhino.Runtime.InProcess.RhinoCore(args);
+
+
+            // NOTE:
+            // ensure RDK and its associated native libraries are ready
+            // rdk.rhp plugin must be loaded before the rdk native library.
+            // a fresh build of rhino on builder machines does not load this
+            string rdkRhp = Path.Combine(Configs.Current.RhinoSystemDir, "Plug-ins", "rdk.rhp");
+            Rhino.PlugIns.PlugIn.LoadPlugIn(rdkRhp, out Guid _);
+
+            Rhino.Runtime.HostUtils.InitializeRhinoCommon_RDK();
         }
 
         public static void LoadEto()
