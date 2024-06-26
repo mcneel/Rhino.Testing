634a5090a6906075829fb96ddf7242bf79861034
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2024-06-26 15:03:53 -0700

    Created a Test Fixture Attribute

---
+++ src/Fixtures/RhinoTestFixtureAttribute.cs
@@ -0,0 +1,28 @@
+using System;
+using NUnit.Framework;
+
+namespace Rhino.Testing.Fixtures
+{
+
+    public class RhinoTestFixtureAttribute : TestFixtureAttribute, IDisposable
+    {
+        /// <summary>Default Constructor</summary>
+        public RhinoTestFixtureAttribute()
+        {
+            Init();
+        }
+
+        /// <summary>Initialises RhinoCore</summary>
+        protected static void Init()
+        {
+            RhinoCore.Initialize();
+        }
+
+        public void Dispose()
+        {
+            RhinoCore.TearDown();
+        }
+
+    }
+
+}
