b4c66daed0c659fd9147627bf4b9dd8edbc0f994
Ehsan Iran-Nejad <eirannejad@gmail.com>
2024-07-17 17:44:57 -0700

    improved plugin loading on macos

--- src/_Loaders/PluginLoader.cs
+++ src/_Loaders/PluginLoader.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
+using RhinoInside;
 using NUnit.Framework;
 
 namespace Rhino.Testing
@@ -11,10 +13,17 @@ namespace Rhino.Testing
     {
         public static IEnumerable<string> GetPluginSearchPaths()
         {
-            const string PLUGINS = "Plug-ins";
-
-            yield return Path.Combine(Configs.Current.RhinoSystemDir, PLUGINS);
-            yield return Path.Combine(Path.GetDirectoryName(Configs.Current.RhinoSystemDir), PLUGINS);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                const string MANAGED_PLUGINS = "RhCore.framework/Versions/A/Resources/ManagedPlugIns";
+                yield return Path.Combine(Configs.Current.RhinoSystemDir, MANAGED_PLUGINS);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                const string PLUGINS = "Plug-ins";
+                yield return Path.Combine(Configs.Current.RhinoSystemDir, PLUGINS);
+                yield return Path.Combine(Path.GetDirectoryName(Configs.Current.RhinoSystemDir), PLUGINS);
+            }
         }
 
         public static string GetRHPPath(string rhpPath)
@@ -25,10 +34,14 @@ namespace Rhino.Testing
                 return rhp;
             }
 
-            rhp = Path.Combine(Path.GetDirectoryName(Configs.Current.RhinoSystemDir), rhpPath);
-            if (File.Exists(rhp))
+            // On Rhino-Windows, walk back up from System path to find plugin path
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return rhp;
+                rhp = Path.Combine(Path.GetDirectoryName(Configs.Current.RhinoSystemDir), rhpPath);
+                if (File.Exists(rhp))
+                {
+                    return rhp;
+                }
             }
 
             throw new FileNotFoundException(rhpPath);
@@ -42,8 +55,19 @@ namespace Rhino.Testing
             // ensure RDK and its associated native libraries are ready
             // rdk.rhp plugin must be loaded before the rdk native library.
             // a fresh build of rhino on builder machines does not load this
-            string rdkRhp = GetRHPPath(@"Plug-ins\rdk.rhp");
-            Rhino.PlugIns.PlugIn.LoadPlugIn(rdkRhp, out Guid _);
+            string rdkRhp;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                rdkRhp = "RhCommonRDK.framework/Versions/A/RhCommonRDK";
+                Rhino.PlugIns.PlugIn.LoadPlugIn(rdkRhp, out Guid _);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                rdkRhp = GetRHPPath(@"Plug-ins\rdk.rhp");
+                Rhino.PlugIns.PlugIn.LoadPlugIn(rdkRhp, out Guid _);
+            }
+            else
+                throw new RhinoInsideInitializationException("Failed loading rdk plugin");
 
             Rhino.Runtime.HostUtils.InitializeRhinoCommon_RDK();
         }
@@ -64,19 +88,40 @@ namespace Rhino.Testing
         {
             TestContext.WriteLine("Loading legacy ironpython");
 
-            PlugIns.LoadPlugInResult res =
-                PlugIns.PlugIn.LoadPlugIn(GetRHPPath(@"Plug-ins\IronPython\RhinoDLR_Python.rhp"), out Guid _);
+            string rhpPath;
+            PlugIns.LoadPlugInResult res = PlugIns.LoadPlugInResult.ErrorUnknown;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                rhpPath = "RhCore.framework/Versions/A/Resources/ManagedPlugIns/RhinoDLR_Python.rhp";
+                res = PlugIns.PlugIn.LoadPlugIn(rhpPath, out Guid _);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                rhpPath = GetRHPPath(@"Plug-ins\IronPython\RhinoDLR_Python.rhp");
+                res = PlugIns.PlugIn.LoadPlugIn(rhpPath, out Guid _);
+            }
 
             if (PlugIns.LoadPlugInResult.Success != res)
-                TestContext.WriteLine("Failed loading legacy ironpython plugin");
+                throw new RhinoInsideInitializationException("Failed loading legacy ironpython plugin");
         }
 
         public static void LoadGrasshopper()
         {
             TestContext.WriteLine("Loading grasshopper");
 
-            PlugIns.LoadPlugInResult res =
-                PlugIns.PlugIn.LoadPlugIn(GetRHPPath(@"Plug-ins\Grasshopper\GrasshopperPlugin.rhp"), out Guid ghId);
+            string rhpPath;
+            PlugIns.LoadPlugInResult res = PlugIns.LoadPlugInResult.ErrorUnknown;
+            Guid ghId = Guid.Empty;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                rhpPath = "RhCore.framework/Versions/A/Resources/ManagedPlugIns/GrasshopperPlugin.rhp";
+                res = PlugIns.PlugIn.LoadPlugIn(rhpPath, out ghId);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                rhpPath = GetRHPPath(@"Plug-ins\Grasshopper\GrasshopperPlugin.rhp");
+                res = PlugIns.PlugIn.LoadPlugIn(rhpPath, out ghId);
+            }
 
             if (PlugIns.LoadPlugInResult.Success == res)
             {
@@ -84,10 +129,10 @@ namespace Rhino.Testing
                 if (ghObj?.GetType().GetMethod("RunHeadless") is MethodInfo runHeadLess)
                     runHeadLess.Invoke(ghObj, null);
                 else
-                    TestContext.WriteLine("Failed loading grasshopper (Headless)");
+                    throw new RhinoInsideInitializationException("Failed loading grasshopper (Headless)");
             }
             else
-                TestContext.WriteLine("Failed loading grasshopper plugin");
+                throw new RhinoInsideInitializationException("Failed loading grasshopper plugin");
         }
 
         public static void LoadPlugins(IEnumerable<string> rhpPaths)
@@ -98,10 +143,10 @@ namespace Rhino.Testing
 
                 TestContext.WriteLine($"Loading plugin from {fullPath}");
 
-                if (PlugIns.PlugIn.LoadPlugIn(fullPath, out Guid _) 
-                            != PlugIns.LoadPlugInResult.Success)
+                if (PlugIns.PlugIn.LoadPlugIn(fullPath, out Guid _)
+                        != PlugIns.LoadPlugInResult.Success)
                 {
-                    TestContext.WriteLine("Failed loading legacy ironpython plugin");
+                    throw new RhinoInsideInitializationException($"Failed loading plugin: {fullPath}");
                 }
             }
         }
